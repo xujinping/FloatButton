7c61f4af83d312780e9f88a159be57daeb4a8c59
xujinping <804677682@qq.com>
2015-04-15 13:27:42 +0800

    添加截屏
    添加截屏

--- app/src/main/java/com/example/xjp/test/MainActivity.java
+++ app/src/main/java/com/example/xjp/test/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.xjp.test;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private Button btn;
-    private RTLayout rtlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,30 +18,5 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-    private void showToast() {
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
